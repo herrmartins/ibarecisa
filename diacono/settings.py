d0dd3ce84e4ae9791105d34ae1aeed4b998162dc
herrmartins <rafaelmagister@gmail.com>
2024-05-29 09:04:59 -0300

    settings changes .env

--- diacono/settings.py
+++ diacono/settings.py
@@ -7,79 +7,78 @@ import mimetypes
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config("SECRET_KEY")
 
-DEBUG = config('DEBUG', cast=bool)
+DEBUG = config("DEBUG", cast=bool)
 
 if not DEBUG:
-    ALLOWED_HOSTS = ['ibarecisa.org.br', '104.251.208.79', '127.0.0.1']
+    ALLOWED_HOSTS = ["ibarecisa.org.br", "104.251.208.79", "127.0.0.1"]
 else:
-    ALLOWED_HOSTS = ['*']
+    ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'phonenumber_field',
-    'ckeditor',
-    'rest_framework',
-    'xhtml2pdf',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "phonenumber_field",
+    "ckeditor",
+    "rest_framework",
+    "xhtml2pdf",
     "weasyprint",
-
-    'blog.apps.BlogConfig',
-    'events.apps.EventsConfig',
-    'core.apps.CoreConfig',
-    'users.apps.UsersConfig',
-    'secretarial.apps.SecretarialConfig',
-    'treasury.apps.TreasuryConfig',
+    "blog.apps.BlogConfig",
+    "events.apps.EventsConfig",
+    "core.apps.CoreConfig",
+    "users.apps.UsersConfig",
+    "secretarial.apps.SecretarialConfig",
+    "treasury.apps.TreasuryConfig",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = 'diacono.urls'
+ROOT_URLCONF = "diacono.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'core.core_context_processor.context_user_data',
-                'django.contrib.auth.context_processors.auth',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "core.core_context_processor.context_user_data",
+                "django.contrib.auth.context_processors.auth",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'diacono.wsgi.application'
+WSGI_APPLICATION = "diacono.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -89,16 +88,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -106,9 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = 'America/Sao_Paulo'
+TIME_ZONE = "America/Sao_Paulo"
 
 USE_I18N = True
 
@@ -119,55 +118,48 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 
-STATIC_ROOT = BASE_DIR / 'static'
-STATIC_URL = 'static/'
-MEDIA_ROOT = BASE_DIR / 'media'
-MEDIA_URL = 'media/'
+STATIC_ROOT = BASE_DIR / "static"
+STATIC_URL = "static/"
+MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_URL = "media/"
 
 
-
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
-
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = 'users.CustomUser'
-LOGOUT_REDIRECT_URL = 'core:home'
-LOGIN_REDIRECT_URL = 'core:home'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "users.CustomUser"
+LOGOUT_REDIRECT_URL = "core:home"
+LOGIN_REDIRECT_URL = "core:home"
 
 CKEDITOR_CONFIGS = {
-    'default': {
-        'skin': 'moono',
-        'toolbar': 'basic',
-        'height': 'full',
-        'width': 'full',
-        'removePlugins': 'exportpdf',
-        'toolbarCanCollapse': 'true'
+    "default": {
+        "skin": "moono",
+        "toolbar": "basic",
+        "height": "full",
+        "width": "full",
+        "removePlugins": "exportpdf",
+        "toolbarCanCollapse": "true",
     },
 }
 
 
-mimetypes.add_type('image/jpeg', '.jpg', strict=True)
-mimetypes.add_type('image/jpeg', '.jpeg', strict=True)
+mimetypes.add_type("image/jpeg", ".jpg", strict=True)
+mimetypes.add_type("image/jpeg", ".jpeg", strict=True)
 mimetypes.init()
 
 
-LANGUAGE_CODE = 'pt-br'
+LANGUAGE_CODE = "pt-br"
 USE_I18N = True
 USE_L10N = True
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-# EMAIL_HOST = "app.debugmail.io"
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = config("EMAIL_HOST")
+EMAIL_HOST_USER = config("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
+EMAIL_PORT = config("EMAIL_PORT")
 EMAIL_USE_TLS = True
 
 
-handler404 = 'core.views.custom_404'
-handler403 = 'core.views.custom_403'
+handler404 = "core.views.custom_404"
+handler403 = "core.views.custom_403"
 
 
 if not DEBUG:
