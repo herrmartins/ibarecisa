fb38b4d41a6852558b0d332cf48ec3ec1cc4bd48
Rafael Dias Martins <rafaelmagister@gmail.com>
2025-02-04 21:11:11 -0300

    fix tests

--- events/tests/events_form_tests.py
+++ events/tests/events_form_tests.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+from django.utils import timezone
 from django.test import TestCase
 from users.models import CustomUser
 from events.models import Event, Venue, EventCategory
@@ -17,8 +19,8 @@ class EventFormTestCase(TestCase):
             "user": self.user.id,
             "title": "Sample Event",
             "description": "A description for the event.",
-            "start_date": "2025-01-01 12:00:00",
-            "end_date": "2025-01-01 14:00:00",
+            "start_date": timezone.now() + timedelta(days=10),
+            "end_date": timezone.now() + timedelta(days=10, hours=2),
             "price": "10.00",
             "location": self.location.id,
             "contact_user": "",
