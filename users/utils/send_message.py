7b07c6c7e414e3475f9459ac1fcf19f9b58a0eea
Rafael Dias Martins <rafaelmagister@gmail.com>
2025-09-13 21:16:45 -0300

    Treasury enhancements

--- users/utils/send_message.py
+++ users/utils/send_message.py
@@ -5,4 +5,4 @@ def send_message(mail_subject="Test",
                  mail_message="Mensagem de teste...",
                  recipient_email=["rafael@rdmartins.adv.br"]):
     send_mail(mail_subject, mail_message,
-              "jusrafaelmartin@gmail.com", recipient_email)
+              "naoresponda@ibarecisa.org.br", recipient_email)
