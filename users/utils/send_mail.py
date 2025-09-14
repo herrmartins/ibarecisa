80c5c0553c0468117fe877c70b8b7ae0d9d704e4
Rafael Dias Martins <rafaelmagister@gmail.com>
2025-09-14 17:06:33 -0300

    email bug fix

--- users/utils/send_mail.py
+++ users/utils/send_mail.py
@@ -3,12 +3,12 @@ from django.core.mail import send_mail
 
 mail_subject = "Test"
 mail_message = "Mensagem..."
-email = "email@asd.com"
-send_mail(mail_subject, mail_message, email, ["rafael@rdmartins.adv.br"])
+from_email = "jusrafaelmartin@gmail.com"
+send_mail(mail_subject, mail_message, from_email, ["rafael@rdmartins.adv.br"])
 
 
 def send_message(mail_subject="Test",
-                 mail_message="Mensagem...",
-                 recipient_email="rafael@rdmartins.adv.br"):
+                  mail_message="Mensagem...",
+                  recipient_email="rafael@rdmartins.adv.br"):
     send_mail(mail_subject, mail_message,
-              "jusrafaelmartin@gmail.com", recipient_email)
+              "jusrafaelmartin@gmail.com", [recipient_email])
